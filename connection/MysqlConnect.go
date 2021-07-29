09c7c3bde62a50658b9818d76e22910166d01804
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-07-29 14:13:40 +0700

    add dockerfile

--- connection/MysqlConnect.go
+++ connection/MysqlConnect.go
@@ -1,6 +1,7 @@
 package connection
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"sync"
@@ -34,7 +35,13 @@ func GetConnection() *MysqlConnection {
 }
 
 func (conn *MysqlConnection) Connect() {
-	dsn := "admin:password@tcp(127.0.0.1:3306)/doremonangis?charset=utf8mb4&parseTime=True&loc=Local"
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	database := os.Getenv("DB_DATABASE")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
